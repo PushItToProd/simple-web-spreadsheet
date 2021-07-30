223173971ecb3b04915e3b1530eebce99d450cc0
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2021-07-30 12:32:58 -0700

    implement basic expression eval with math.js

--- poc/worker.js
+++ poc/worker.js
@@ -1,14 +1,34 @@
-// self.importScripts("math.js");
+self.importScripts("math.js");
 
+
+function value(n) {
+  let x = +n;
+  if (n !== x.toString())
+    return n;
+  return x;
+}
 
 self.onmessage = function(message) {
   sheet = message.data || {};
   vals = {};
 
-  // for (let coord in sheet) {
-  //   // TODO
-  // }
+  for (let coord in sheet) {
+    let sheetVal = sheet[coord];
 
-  // postMessage({vals});
-  postMessage({vals: sheet})
+    if (sheetVal[0] !== '=') {
+      vals[coord] = value(sheetVal);
+      continue;
+    }
+
+    let formula = sheetVal.slice(1);
+
+    try {
+      vals[coord] = math.evaluate(formula);
+    } catch (e) {
+      console.error("eval failed at coordinate", coord, "with error", e);
+      vals[coord] = {error: e.toString()};
+    }
+  }
+
+  postMessage({vals});
 };
