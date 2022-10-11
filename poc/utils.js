0ae6b2f051c61d304ecf9702d6ad0bb7c4a07f39
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2022-10-10 17:29:30 -0700

    simplify getting type name

--- poc/utils.js
+++ poc/utils.js
@@ -1,8 +1,5 @@
 export function getType(o) {
-  // Object.prototype.toString.call returns a string like '[object ClassName]',
-  // so we slice off the leading '[object ' and trailing ']' to get the plain
-  // name
-  return Object.prototype.toString.call(o).slice(8, -1);
+  return o.constructor.name;
 }
 
 export function isString(o) {
